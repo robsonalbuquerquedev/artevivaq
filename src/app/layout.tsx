442653f532ff7d48ff4ece2e2213ede84d2bf919
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-11-09 01:40:23 -0300

    update: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ApoieCard from "@/components/ApoieCard";
 
 export const metadata: Metadata = {
   title: "ArteVivaQ | Galeria de Desenhos",
@@ -52,6 +53,7 @@ export default function RootLayout({
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <ApoieCard />
       </body>
     </html>
   );
